c2d0df3363b5bca9b2c65b39a461d55e62ff0a80
geno <genad.papashvili@gmail.com>
2024-01-29 00:59:11 +0400

    update

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -2,30 +2,42 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {catchError, tap} from "rxjs/operators";
-import {map, Observable, of} from "rxjs";
+import {BehaviorSubject, map, Observable, of} from "rxjs";
 
 export type Package = { error: string | null }
+
+export enum SessionStatus {
+  created = 1, removed = 0
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor(private http: HttpClient) {
+  private sessionSubject: BehaviorSubject<SessionStatus>
 
+  constructor(private http: HttpClient) {
+    const sessionStatus = window.sessionStorage.getItem("token") ? SessionStatus.created : SessionStatus.removed
+    this.sessionSubject = new BehaviorSubject(sessionStatus)
+  }
+
+  public sessionChanges() {
+    return this.sessionSubject.asObservable();
   }
 
   create(client: { email: string, password: string }): Observable<Package> {
     return this.http.post<{ token: string }>(environment.apiUrl + "/login", client)
       .pipe(
-        tap(this.saveToken),
+        tap(token => this.saveToken(token)),
         map(() => ({error: null})),
-        catchError((it) => of({error: it.error.error}))
+        catchError((it) => of({error: it.error?.error}))
       );
   }
 
-  public delete() {
+  public remove() {
     window.sessionStorage.removeItem("token")
+    this.sessionSubject.next(SessionStatus.removed)
   }
 
   public getToken() {
@@ -34,5 +46,6 @@ export class SessionService {
 
   private saveToken(it: { token: string }) {
     window.sessionStorage.setItem("token", it.token)
+    this.sessionSubject.next(SessionStatus.created)
   }
 }
